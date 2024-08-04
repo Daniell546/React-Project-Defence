f2a369c664af07f767573bc520bf053ff4cabd10
Daniell546 <99045024+Daniell546@users.noreply.github.com>
2024-08-04 14:44:59 +0300

    prepare for heroku deployment

---
+++ server/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    fallback: {
+      "stream": require.resolve("stream-browserify"),
+      "path": require.resolve("path-browserify")
+    }
+  },
+  
+  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  mode: 'production',
+};
